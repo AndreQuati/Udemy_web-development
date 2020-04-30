6dc1e013bff076877da4c517757bbd4c7e1afc52
AndreQuati <50759145+AndreQuati@users.noreply.github.com>
2020-04-30 14:43:10 +0900

    up to challenge 19

--- 16-Blog_Website/app.js
+++ 16-Blog_Website/app.js
@@ -1,20 +1,30 @@
 //jshint esversion:6
 
+// Requiring express, body parser and ejs
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+// Requiring lodash - library for multiple kind of operations, in this case for working with 
+// strings from client requests.
+const _ = require("lodash"); //lodash full build (other custom builds are available)
+
+
+// Initial content of home / about / contact screens
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const blogPosts = [];
 
+// Initiatlizing express in 'app'
 const app = express();
 
+// Initializing EJS and Body Parser
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
+// Setting folder 'public' as static to be accessible by the client (browser)
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
@@ -50,13 +60,19 @@ app.post("/compose", function(req, res){
 // is "user/:userid", req.params will return {userid: "2090"}
 app.get("/posts/:postName", function(req, res){
   //Regardless of the name of the parameter or how many there are, they are accessed by this "req.params"
-  const postName = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   blogPosts.forEach(function(post){
-    if(post.title == postName){
+
+    const postTitle = _.lowerCase(post.title);
+
+    if(postTitle === requestedTitle){
       res.render("post", {postTitle: post.title, postBody: post.body});
+    } else {
+      console.log("Not a match");
     }
   });
+
 });
 
 app.listen(3000, function() {
